77cbd9463ce2df72509e824363b419990d8c542e
successJohn <87600912+successJohn@users.noreply.github.com>
2021-12-11 22:53:02 +0100

    Setup: Uploading Images to Blog Website

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//const http = require("http");
+
 const path = require("path");
 const express = require("express");
 const app = express();
@@ -6,11 +6,14 @@ const ejs = require("ejs");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const BlogPost = require("./models/blogpost");
+const fileUpload = require("express-fileupload");
+
 app.set("view engine", "ejs");
 
 app.use(express.static("Public"));
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
+app.use(fileUpload());
 mongoose.connect("mongodb://localhost/blogDB", {useNewUrlParser: true})
 
 app.get("/", async(req,res)=>{
@@ -25,8 +28,14 @@ app.get("/about", (req,res)=>{
 app.get("/contact", (req,res)=>{
     res.render("contact")
 })
-app.get("/post", (req,res)=>{
-    res.render("post")
+
+app.get("/post/:id",async(req,res)=>{
+    const blogpost = await BlogPost.findById(`${req.params.id}`);
+   
+    //console.log(blogpost);
+     res.render("post",{
+         blogpost
+     });
 })
 
 app.get("/posts/new",(req,res)=>{
@@ -35,13 +44,17 @@ app.get("/posts/new",(req,res)=>{
 
 
 app.post("/posts/store", async(req,res)=>{
-    //console.log(req.body);
-    await BlogPost.create(req.body,(error, blogpost)=>{
+  let image = req.files.image;
+  image.mv(path.resolve(__dirname, "Public/assets/img", image.name),async (error)=>{
+    await BlogPost.create({
+        ...req.body,
+        image: "/img/" + image.name})
         res.redirect("/");
     })
+  });
+  
     
-})
-app.listen(8080, ()=>{
+app.listen(4080, ()=>{
     console.log("server listening")
 })
 
